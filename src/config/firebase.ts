768958f941c1fd2ba276340b70439e757b5ef96c
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-04-05 15:41:43 +0300

    set up firebase hosting

---
+++ src/config/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6KUQp76kpcJCtvGcZKP1UT-m29BzGRlA",
+  authDomain: "photoapp-5b23c.firebaseapp.com",
+  projectId: "photoapp-5b23c",
+  storageBucket: "photoapp-5b23c.appspot.com",
+  messagingSenderId: "942847437557",
+  appId: "1:942847437557:web:820caee437a57fb50bb3bc",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
