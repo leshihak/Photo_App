ed4679db9b3d33485db49aaafcf61f0c3530602b
Nataliia <natalyleshihak@gmail.com>
2022-04-07 16:23:07 +0300

    fix layout

--- src/components/ui/DashboardWrapper/DashboardWrapper.tsx
+++ src/components/ui/DashboardWrapper/DashboardWrapper.tsx
@@ -4,18 +4,18 @@ import { Outlet } from "react-router-dom";
 import AppBar from "../AppBar/AppBar";
 
 const DashboardWrapper: FC = () => (
-  <>
+  <Box height="calc(100vh - 60px)">
     <AppBar />
     <Box
       width={1}
+      height="calc(100% - 30px)"
       maxWidth="975px"
       margin="64px auto"
       padding="30px 20px 0"
-      height="calc(100vh - 160px)"
     >
       <Outlet />
     </Box>
-  </>
+  </Box>
 );
 
 export default DashboardWrapper;
--- src/components/ui/ItemGrid/ItemGrid.tsx
+++ src/components/ui/ItemGrid/ItemGrid.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { FC, useState } from "react";
 import { ImageType } from "../../../models/ui.model";
 import FavoriteIcon from "@mui/icons-material/Favorite";
@@ -8,74 +8,69 @@ interface ItemGridProps {
   items: ImageType[];
 }
 
-const ItemGrid: FC<ItemGridProps> = ({ items }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  return (
-    <Grid item xs={12}>
-      <Grid container justifyContent="center" spacing={3.5}>
-        {items.map((item) => (
-          <Grid
-            key={item.id}
-            item
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            style={{
-              objectFit: "cover",
-              height: "321px", // need to be 293px
-              width: "321px", // need to be 293px
-              position: "relative",
-            }}
-          >
-            <img
-              alt={item.alt}
-              src={item.src}
-              style={{
-                objectFit: "cover",
-                height: "100%",
-                width: "100%",
-              }}
-            />
-            {isHovered && (
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="center"
-                position="absolute"
-                top="28px"
-                width="91%"
-                height="91%"
-                sx={{ backgroundColor: "rgb(58 58 58 / 59%)" }}
-              >
-                <Box display="flex" alignItems="center">
-                  <FavoriteIcon sx={{ color: "white", mr: 0.5 }} />
-                  <Typography
-                    align="center"
-                    variant="body2"
-                    fontWeight="bold"
-                    sx={{ color: "white" }}
-                  >
-                    19
-                  </Typography>
-                </Box>
-                <Box display="flex" alignItems="center" ml={3}>
-                  <ChatBubbleIcon sx={{ color: "white", mr: 0.5 }} />
-                  <Typography
-                    align="center"
-                    variant="body2"
-                    fontWeight="bold"
-                    sx={{ color: "white" }}
-                  >
-                    0
-                  </Typography>
-                </Box>
-              </Box>
-            )}
-          </Grid>
-        ))}
-      </Grid>
-    </Grid>
-  );
-};
+const ItemGrid: FC<ItemGridProps> = ({ items }) => (
+  <Box display="flex" flexWrap="wrap" justifyContent="space-between">
+    {items.map((item) => (
+      <Box
+        key={item.id}
+        mb={4}
+        sx={{
+          objectFit: "cover",
+          height: "293px",
+          width: "293px",
+          position: "relative",
+          "&:hover .hidden-block": {
+            display: "flex",
+            cursor: "pointer",
+          },
+        }}
+      >
+        <img
+          alt={item.alt}
+          src={item.src}
+          style={{
+            objectFit: "cover",
+            height: "100%",
+            width: "100%",
+          }}
+        />
+        <Box
+          display="none"
+          className="hidden-block"
+          alignItems="center"
+          justifyContent="center"
+          position="absolute"
+          top={0}
+          width={1}
+          height={1}
+          sx={{ backgroundColor: "rgb(58 58 58 / 59%)" }}
+        >
+          <Box display="flex" alignItems="center">
+            <FavoriteIcon sx={{ color: "white", mr: 0.5 }} />
+            <Typography
+              align="center"
+              variant="body2"
+              fontWeight="bold"
+              sx={{ color: "white" }}
+            >
+              19
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" ml={3}>
+            <ChatBubbleIcon sx={{ color: "white", mr: 0.5 }} />
+            <Typography
+              align="center"
+              variant="body2"
+              fontWeight="bold"
+              sx={{ color: "white" }}
+            >
+              0
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    ))}
+  </Box>
+);
 
 export default ItemGrid;
